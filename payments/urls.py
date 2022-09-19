1eac4b68a1d781bd988c83e52475ebb698c1e64f
Anatoliy Kunshin <reyiby2015@gmail.com>
2022-09-19 16:09:51 +0300

    Working with checkout_session

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ItemView, ItemBuyView
+from .views import ItemView, sync_products_view, ItemBuyView
 
 
 urlpatterns = [
+    path('', sync_products_view),
     path('item/<int:pk>/', ItemView.as_view(), name="view_item"),
     path('buy/<int:pk>/', ItemBuyView.as_view(), name="buy_item"),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,15 +1,33 @@
 import stripe
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import DetailView, View
 from django.conf import settings
 from shop.models import Item
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+products = stripe.Product.list()
+prices = stripe.Price.list()
+
+
+def sync_products_view(request):
+    '''Синхронизация товаров с stripe в БД'''
+    print(products)
+    print(prices)
+    # Приведение цены к float
+    for prod in products:
+        price_ = [x for x in prices.data if x.product == prod.id][0]
+        price = float(price_.unit_amount / 100)
+        print(price)
+
+        obj, _ = Item.objects.get_or_create(name=prod.name)
+        obj.description = prod.description
+        obj.price = price
+        obj.save()
+    return HttpResponse("data")
 
 class ItemView(DetailView):
     model = Item
-    # template_name = "base.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,35 +35,34 @@ class ItemView(DetailView):
         return context
 
 
+
 class ItemBuyView(View):
     def create_checkout_session(request, item_id):
         item = Item.objects.get(pk=item_id)
-        if request.method == 'GET':
-            domain_url = 'http://localhost:8000/'
-            try:
-                # Create new Checkout Session for the order
-                # Other optional params include:
-                # [billing_address_collection] - to display billing address details on the page
-                # [customer] - if you have an existing Stripe Customer ID
-                # [payment_intent_data] - capture the payment later
-                # [customer_email] - prefill the email input in the form
-                # For full details see https://stripe.com/docs/api/checkout/sessions/create
+        domain_url = 'http://localhost:8000/'
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'success.html',
+                cancel_url=domain_url + 'cancel.html',
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[{
+                'price_data': {
+                    'product_data': {
+                        'name': item.name,
+                    },
+                    'unit_amount': item.price
+                },
+                'quantity': 1,
+                }],
+                # line_items=[
+                #     {
+                #         'name': item.name,
+                #         'amount': item.price,
+                #     }
+                # ]
+            )
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
 
-                # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-                checkout_session = stripe.checkout.Session.create(
-                    success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                    cancel_url=domain_url + 'cancelled/',
-                    payment_method_types=['card'],
-                    mode='payment',
-                    line_items=[
-                        {
-                            'name': item.name,
-                            'quantity': 1,
-                            'currency': 'rub',
-                            'amount': item.price,
-                        }
-                    ]
-                )
-                return JsonResponse({'sessionId': checkout_session['id']})
-            except Exception as e:
-                return JsonResponse({'error': str(e)})
+        return JsonResponse({'sessionId': checkout_session['id']})
