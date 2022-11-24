ebde38279e754f2cd167696b946b66cdcd4dd9b0
Anatoliy Kunshin <reyiby2015@gmail.com>
2022-11-24 13:48:46 +0300

    Edit url in templates

--- payments/views.py
+++ payments/views.py
@@ -14,6 +14,8 @@ customer = stripe.Customer.create()
 products = stripe.Product.list()
 prices = stripe.Price.list()
 
+domain_url = "http://localhost:8000/"
+
 
 class AllItemsView(ListView):
     """Вывод списка товаров"""
@@ -49,7 +51,7 @@ class ItemView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['stripe_pub_key'] = settings.STRIPE_PUBLISHABLE_KEY
+        context["stripe_pub_key"] = settings.STRIPE_PUBLISHABLE_KEY
         return context
 
 
@@ -58,28 +60,27 @@ class ItemBuyView(View):
 
     def get(self, request, *args, **kwargs):
         item_id = self.kwargs["pk"]
-        item = Item.objects.get(pk=item_id)
-        domain_url = 'http://localhost:8000/'
+        item = Item.objects.get(pk=item_id)        
         try:
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success/',
-                cancel_url=domain_url + 'cancel/',
-                mode='payment',
+                success_url=domain_url + "success/",
+                cancel_url=domain_url + "cancel/",
+                mode="payment",
                 line_items=[{
-                    'price_data': {
-                        'currency': item.currency,
-                        'product_data': {
-                            'name': item.name,
+                    "price_data": {
+                        "currency": item.currency,
+                        "product_data": {
+                            "name": item.name,
                         },
-                        'unit_amount': int(item.price * 100),
+                        "unit_amount": int(item.price * 100),
                     },
-                    'quantity': 1,
+                    "quantity": 1,
                 }],
             )
         except Exception as e:
-            return JsonResponse({'error': str(e)})
+            return JsonResponse({"error": str(e)})
 
-        return JsonResponse({'sessionId': checkout_session['id']})
+        return JsonResponse({"sessionId": checkout_session["id"]})
 
 
 class SuccessPayView(TemplateView):
@@ -94,14 +95,14 @@ class CancelPayView(TemplateView):
 
 def add_to_order(request):
     """Форма создания заказа"""
-    if request.method == 'POST':
+    if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             order = form.save()
             return redirect("orders_list")
     else:
         form = OrderForm()
-    return render(request, "shop/add_to_order.html", {'form': form})
+    return render(request, "shop/add_to_order.html", {"form": form})
 
 
 class OrderPaymentView(TemplateView):
@@ -118,7 +119,8 @@ class OrderPaymentView(TemplateView):
             "items": items,
             "order": order,
             "clientSecret": stripe_pub_key,
-            "stripe_pub_key": stripe_pub_key
+            "stripe_pub_key": stripe_pub_key,
+            "domain_url": domain_url
         })
         return context
 
@@ -126,26 +128,26 @@ class OrderPaymentView(TemplateView):
 class StripeIntentView(View):
     """Создание PaymntIntent - загрузка формы оплаты на страницу"""
     def post(self, request, *args, **kwargs):
-        if request.method == 'POST':
+        if request.method == "POST":
             try:
                 req_json = json.loads(request.body)
-                items = req_json['items']
+                items = req_json["items"]
                 order_id = self.kwargs["pk"]
                 order = Order.objects.get(id=order_id)
 
                 intent = stripe.PaymentIntent.create(
                     amount=int(order.get_total_price * 100),
-                    currency='rub',
+                    currency="rub",
                     automatic_payment_methods={
-                        'enabled': True,
+                        "enabled": True,
                     },
-                    customer=customer['id'],
+                    customer=customer["id"],
                     metadata={
                         "order_id": order.id
                     }
                 )
                 return JsonResponse({
-                    'clientSecret': intent['client_secret']
+                    "clientSecret": intent["client_secret"]
                 })
             except Exception as e:
-                return JsonResponse({'error': str(e)})
+                return JsonResponse({"error": str(e)})
